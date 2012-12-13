69e57768e007e8edd9d21b51ad0c0d575d7cd94f
Vyacheslav Boychenko <massive.kinetics@gmail.com>
2012-12-13 22:05:35 +0200

    Player page

--- src/com/massivekinetics/emp/gui/fragments/PlayerFragment.java
+++ src/com/massivekinetics/emp/gui/fragments/PlayerFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.massivekinetics.emp.EMPApplication;
 import com.massivekinetics.emp.R;
 
 public class PlayerFragment extends SherlockFragment {
+	SeekBar bar;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,33 @@ public class PlayerFragment extends SherlockFragment {
 	}
 
 	@Override
+	public void onResume() {
+		super.onResume();
+		new Thread() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 100; i++) {
+					bar.setProgress(i);
+					synchronized (this) {
+						try {
+
+							wait(200);
+
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}.start();
+	}
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.player_page, null);
+		View layout = inflater.inflate(R.layout.player_page, null);
+		bar = (SeekBar) layout.findViewById(R.id.seekBar);
+		return layout;
 	}
 }
